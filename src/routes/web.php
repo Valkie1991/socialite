b114c2c77ad5d4125e44c2b51a7099dd771fc1b5
Henri <valkhenri1991@gmail.com>
2020-09-10 16:33:50 +0200

    testing

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 Route::group([
     'namespace' => 'Henri\Socialite\Http\Controllers',
-    'middleware' => 'web'
+    'middleware' => ['web']
 ], function () {
     Route::get('/login/gitlab', 'GitlabController@redirectToProvider')->name('login.gitlab');
     Route::get('/login/gitlab/callback', 'GitlabController@handleProviderCallback')->name('login.gitlab.callback');
